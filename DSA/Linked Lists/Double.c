4408e4e89c6640de34d0b9256d4ec202769d61f0
azammustafa66 <azammustafa66@gmail.com>
2023-03-21 11:31:44 +0530

    C

---
+++ DSA/Linked Lists/Double.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+    struct node *prev;
+    int data;
+    struct node *next;
+}*head = NULL;
+
+
+void create() {
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+
+    printf("Enter the data: ");
+    scanf("%d", &temp->data);
+    temp->next = NULL;
+
+    if (head == NULL) {
+        temp->prev = NULL;
+        head = temp;
+        return;
+    } else {
+        struct node *ptr = head;
+
+        while (ptr->next != NULL) {
+            ptr = ptr->next;
+        }
+        ptr->next = temp;
+    }
+}
+
+void display() {
+    struct node *temp = head;
+
+    while (temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+}
+
+void insert_beg() {
+    struct node *temp = (struct node *)malloc(sizeof(struct node));
+        
+    printf("Enter the data: ");
+    scanf("%d",&temp->data);
+    temp->next = NULL;
+
+    if (head == NULL) {
+        head = temp;
+        return;
+    } else {
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
+}
+
+int main() {
+    create();
+    insert_beg();
+
+    display();
+
+    return 0;
+}
